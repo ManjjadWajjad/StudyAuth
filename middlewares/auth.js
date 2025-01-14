c7742622361339a4b6d1fa00a4813e3d9bc84135
ManjjadWajjad <muhammadfaisalmuzhaffar@gmail.com>
2025-01-14 09:24:30 +0700

    Update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,18 @@
 const jwt = require('jsonwebtoken')
 
-const auth = (req,res,next)=>{
-    const  token = req.header("Authorization")
-    if (1token) {
+const auth = (req,res,next) => {
+    const token = req.header('Authorization')
+    if(!token){
         return
+        res.status(401).json({message: 'unauthorized'})
+
     }
-}
+    jwt.verify(token, process.env.SECRET_KEY, (eror, user) => {
+        if(eror){
+            return res.json({message: 'invalid token'})
+        }
+      
+        next()
+    })
+}    
+module.exports = auth
